a239740136f9d795f038dcbd5a6a7dddd912768a
Nicolay Makukhin <nnmakukhin@gmail.com>
2020-02-06 23:34:20 +0300

    Добавьте файлы проекта.

---
+++ TestWpfDraw/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestGit
+{
+    class Command : ICommand
+    {
+        public Action<object> Action { get; }
+
+        public bool CanExecute(object parameter) => Action != null;
+
+        public void Execute(object parameter) => Action?.Invoke(parameter);
+
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<object> action) => Action = action;
+    }
+}
---
+++ TestWpfDraw/ViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace TestGit
+{
+    internal class ViewModel
+    {
+        public ObservableCollection<ushort> Data { get; }
+
+        public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+
+        private readonly DispatcherTimer _timer;
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<ushort>();
+            for (var i = 0; i < 512; i++)
+                Data.Add((ushort)i);
+
+            _timer = new DispatcherTimer(
+                    TimeSpan.FromMilliseconds(20),
+                    DispatcherPriority.Render,
+                    TimerTick,
+                    Dispatcher.CurrentDispatcher)
+                {IsEnabled = false};
+
+            StartCommand = new Command(o =>
+            {
+                _timer.Start();
+            });
+
+            StopCommand = new Command(o =>
+            {
+                _timer.Stop();
+            });
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            for (var i = 0; i < 512; i++)
+                Data[i]++;
+        }
+    }
+}
